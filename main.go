2a31f4cf3a94a5eb6a0c2be8d435b92d9963acbc
12awoodward <203260994+12awoodward@users.noreply.github.com>
2026-05-03 16:59:23 +0100

    update endpoints

--- main.go
+++ main.go
@@ -20,9 +20,11 @@ func main() {
 	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("GET /healthz", readinessEndpoint)
-	mux.HandleFunc("GET /metrics", apiCfg.metricsEndpoint)
-	mux.HandleFunc("POST /reset", apiCfg.resetMetricsEndpoint)
+
+	mux.HandleFunc("GET /api/healthz", readinessEndpoint)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsEndpoint)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetricsEndpoint)
 
 	err := server.ListenAndServe()
 	if err != nil {
@@ -46,9 +48,9 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsEndpoint(resp http.ResponseWriter, req *http.Request) {
-	hitCount := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	hitCount := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
 
-	resp.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	resp.Header().Add("Content-Type", "text/html; charset=utf-8")
 	resp.WriteHeader(http.StatusOK)
 	resp.Write([]byte(hitCount))
 }
