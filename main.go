0046d8277a04eb3500b42ae4f28322419b1a68a6
12awoodward <203260994+12awoodward@users.noreply.github.com>
2026-05-03 13:00:56 +0100

    fileserver path change & readiness endpoint

--- main.go
+++ main.go
@@ -12,10 +12,17 @@ func main() {
 		Addr: ":8080",
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessEndpoint)
 
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func readinessEndpoint(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	resp.WriteHeader(http.StatusOK)
+	resp.Write([]byte("OK"))
 }
